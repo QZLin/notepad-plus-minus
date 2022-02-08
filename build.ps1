b35fd683b5d9315a54ae25afe329923c9f1855da
QZLin <qzlin01@163.com>
2022-02-08 12:23:02 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
-$VSVCVAR_BAT = "A:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+# $VSVCVAR_BAT = "A:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 Push-Location $PSScriptRoot
-. cmd-pwsh-bridge\bridge.ps1
-Set-Location PowerEditor\visual.net
-SourceCmd($VSVCVAR_BAT)
+# . cmd-pwsh-bridge\bridge.ps1
+# Set-Location PowerEditor\visual.net
+# SourceCmd($VSVCVAR_BAT)
 
 # msbuild .\notepadPlus.sln -t:Clean
-msbuild .\notepadPlus.sln -p:Configuration=Release
+msbuild PowerEditor\visual.net\notepadPlus.sln -p:Configuration=Release
 Pop-Location
